35773a8ef873a70275bc18deb296a093dcf07db9
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-04 02:22:37 -0800

    Update swaybar config

--- home/dotfiles/i3status-rust.nix
+++ home/dotfiles/i3status-rust.nix
@@ -2,28 +2,30 @@
 
 {
   xdg.configFile."i3status-rust/config.toml".text = ''
+    icons_format = ""
+
+    [theme]
     theme = "native"
-    icons = "awesome6"
+
+    [icons]
+    icons = "none"
 
     [[block]]
     block = "net"
-    format = " {ssid} {signal_strength}% {ip} "
-    format_alt = " {device} "
-
-    [[block]]
-    block = "sound"
+    format = " $signal_strength $ssid via $device "
+    format_alt = " $device "
 
     [[block]]
     block = "battery"
-    format = " {percentage}% "
-    format_charging = " {percentage}% (chg) "
-    format_full = " {percentage}% (full) "
-
-    [[block]]
-    block = "backlight"
+    driver = "sysfs"
+    device = "BAT0"
+    format = " $percentage% "
+    format_charging = " $percentage% (chg) "
+    format_full = " $percentage% (full) "
+    missing_format = " "
 
     [[block]]
     block = "time"
-    format = " %a %b %d %H:%M "
+    format = " $timestamp.datetime(f:'%a %b %d %H:%M') "
   '';
 }
