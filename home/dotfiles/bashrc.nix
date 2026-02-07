e411255377f8ffe3d925051b6cb0740f8d60d568
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-06 17:35:36 -0800

    Update to use direnv and update color for terminal

--- home/dotfiles/bashrc.nix
+++ home/dotfiles/bashrc.nix
@@ -6,12 +6,21 @@
     enableCompletion = true;
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      eval "$(direnv hook bash)"
       PS1="\[\e[38;2;0;199;129m\]\u@\h:\w\[\e[0m\]\$ "
+      if [[ -n "$IN_NIX_SHELL" ]]; then
+        PS1="(nix-shell) \[\e[38;2;0;199;129m\]\u@\h:\w\[\e[0m\]\$ "
+      fi
       if [[ $- == *i* ]]; then
         bind '"\C-?": backward-kill-word'
         bind '"\C-h": backward-kill-word'
       fi
     '';
     shellAliases = { };
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
   };
 }
