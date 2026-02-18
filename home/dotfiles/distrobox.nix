83580adf564f573a3040f2e4a1e0d1c79a0ea394
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-18 14:37:01 -0800

    Fix distrobox config

--- home/dotfiles/distrobox.nix
+++ home/dotfiles/distrobox.nix
@@ -4,7 +4,7 @@
   xdg.configFile."distrobox/distrobox.ini".text = ''
     [ubuntu-24-04]
     image=ubuntu:24.04
-    home=%h/Virtualization/home/ubuntu-24-04
+    home=/home/george-sleen/Virtualization/home/ubuntu-24-04
   '';
 
   home.activation.distroboxHome = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
@@ -12,8 +12,8 @@
   '';
 
   home.activation.distroboxAssemble = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if ! download= ${pkgs.distrobox}/bin/distrobox assemble create --replace --file "$HOME/.config/distrobox/distrobox.ini"; then
-      echo "home-manager: distrobox assemble failed; run it manually after rebuild" >&2
-    fi
+    export PATH=${pkgs.podman}/bin:${pkgs.docker}/bin:$PATH
+    export DISTROBOX_CONTAINER_MANAGER=podman
+    ${pkgs.distrobox}/bin/distrobox assemble create --replace --file "$HOME/.config/distrobox/distrobox.ini"
   '';
 }
