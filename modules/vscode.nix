e678e429973fc205420e9b7ff483e6a6d1d42957
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-02-17 21:27:15 -0800

    Switch to FHS so that AI can work

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -11,20 +11,41 @@ let
       sha256 = "sha256-gYyIVnXG9Atmik0c1FsRKO2idFnufwl26nOiH3DYPLY=";
     };
   };
+
+  vscodeExtensions = [
+    helixEmulation
+    pkgs.vscode-extensions.ms-toolsai.jupyter
+    pkgs.vscode-extensions.ms-toolsai.jupyter-keymap
+    pkgs.vscode-extensions.ms-toolsai.jupyter-renderers
+    pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
+    pkgs.vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
+    pkgs.vscode-extensions.ms-python.python
+    pkgs.vscode-extensions.ms-python.vscode-pylance
+  ];
+
+  vscodeExtBundle = pkgs.vscode-with-extensions.override {
+    vscode = pkgs.vscode-fhs;
+    vscodeExtensions = vscodeExtensions;
+  };
+
+  codeWithWritableExtensions = pkgs.writeShellScriptBin "code" ''
+    set -e
+    ext_src="${vscodeExtBundle}/share/vscode/extensions"
+    ext_dst="''${XDG_DATA_HOME:-$HOME/.local/share}/vscode/extensions"
+    mkdir -p "$ext_dst"
+    if [ -d "$ext_src" ]; then
+      for d in "$ext_src"/*; do
+        name="$(basename "$d")"
+        if [ ! -e "$ext_dst/$name" ]; then
+          ln -s "$d" "$ext_dst/$name"
+        fi
+      done
+    fi
+    exec ${pkgs.vscode-fhs}/bin/code --extensions-dir "$ext_dst" "$@"
+  '';
 in {
-  environment.systemPackages = with pkgs; [
-    (vscode-with-extensions.override {
-      vscode = vscode;
-      vscodeExtensions = [
-        helixEmulation
-        vscode-extensions.ms-toolsai.jupyter
-        vscode-extensions.ms-toolsai.jupyter-keymap
-        vscode-extensions.ms-toolsai.jupyter-renderers
-        vscode-extensions.ms-toolsai.vscode-jupyter-cell-tags
-        vscode-extensions.ms-toolsai.vscode-jupyter-slideshow
-        vscode-extensions.ms-python.python
-        vscode-extensions.ms-python.vscode-pylance
-      ];
-    })
+  environment.systemPackages = [
+    codeWithWritableExtensions
+    pkgs.vscode-fhs
   ];
 }
