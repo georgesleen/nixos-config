53111268418991bfedb8c1c6d44519d65ce03dd0
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-03-02 23:01:55 -0800

    Let the battery charge a little more

--- hosts/gs-thinkpad-t480s/power.nix
+++ hosts/gs-thinkpad-t480s/power.nix
@@ -24,8 +24,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT0 = 80;
+      STOP_CHARGE_THRESH_BAT0 = 85;
     };
   };
   services.power-profiles-daemon.enable = false;
