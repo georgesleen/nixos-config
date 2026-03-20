14204b0f200b8c019ef783360a17128b67597575
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-03-19 23:32:02 -0700

    Exclude huge vm file

--- home/dotfiles/rclone.nix
+++ home/dotfiles/rclone.nix
@@ -1,7 +1,83 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  localPath = "/home/george-sleen/Shared/classes";
+  remotePath = "gdrive:_laptop/classes";
+  filtersFile = "/home/george-sleen/.config/rclone/excludes.txt";
+  commonArgs = [
+    "bisync"
+    localPath
+    remotePath
+    "--filters-file"
+    filtersFile
+    "--create-empty-src-dirs"
+    "--resilient"
+    "--recover"
+    "--max-lock"
+    "2m"
+    "--drive-skip-gdocs"
+    "--verbose"
+  ];
+  classesBisync = pkgs.writeShellApplication {
+    name = "rclone-classes-bisync";
+    runtimeInputs = [ pkgs.rclone ];
+    text = ''
+      exec rclone ${lib.escapeShellArgs commonArgs}
+    '';
+  };
+  classesResync = pkgs.writeShellApplication {
+    name = "rclone-classes-resync";
+    runtimeInputs = [ pkgs.rclone ];
+    text = ''
+      exec rclone ${lib.escapeShellArgs (commonArgs ++ [ "--resync" ])}
+    '';
+  };
+in
 {
-  home.packages = [ pkgs.rclone ];
+  home.packages = [
+    pkgs.rclone
+    classesBisync
+    classesResync
+  ];
 
   xdg.configFile."rclone/excludes.txt".source = ./rclone/excludes.txt;
+
+  systemd.user.services.rclone-classes = {
+    Unit = {
+      Description = "Bisync classes with Google Drive";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${classesBisync}/bin/rclone-classes-bisync";
+    };
+  };
+
+  systemd.user.services.rclone-classes-resync = {
+    Unit = {
+      Description = "Resync classes with Google Drive";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${classesResync}/bin/rclone-classes-resync";
+    };
+  };
+
+  systemd.user.timers.rclone-classes = {
+    Unit = {
+      Description = "Run Google Drive bisync for classes every 5 minutes";
+    };
+    Timer = {
+      OnBootSec = "2m";
+      OnUnitActiveSec = "5m";
+      Persistent = true;
+      Unit = "rclone-classes.service";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
 }
