dc4a9f542e86753b3b05d5ca0786ebeec080d66c
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-05-11 14:18:00 -0700

    Add tuis to tofi

--- home/dotfiles/packages.nix
+++ home/dotfiles/packages.nix
@@ -8,6 +8,27 @@
     VISUAL = "hx";
   };
 
+  xdg.desktopEntries = {
+    sound = {
+      name = "Sound";
+      exec = "kitty -e pulsemixer";
+      terminal = false;
+      categories = [ "Audio" ];
+    };
+    bluetooth = {
+      name = "Bluetooth";
+      exec = "kitty -e bluetuith";
+      terminal = false;
+      categories = [ "Settings" ];
+    };
+    network = {
+      name = "Network";
+      exec = "kitty -e nmtui";
+      terminal = false;
+      categories = [ "Network" ];
+    };
+  };
+
   home.packages = with pkgs; [
     nil # Nix LSP
     tofi
