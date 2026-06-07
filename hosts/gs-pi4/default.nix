0411e6a7c4976e90e825dee107f9bcf384c528cc
George Sleen <147893275+georgeSleen@users.noreply.github.com>
2026-06-07 00:14:37 -0700

    Fix build error for making the sd card image

--- hosts/gs-pi4/default.nix
+++ hosts/gs-pi4/default.nix
@@ -3,11 +3,13 @@
   pkgs,
   inputs,
   user,
+  modulesPath,
   ...
 }:
 {
   imports = [
     inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    (modulesPath + "/installer/sd-card/sd-image-aarch64.nix")
   ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
